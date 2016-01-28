dd864133b5df7295978798c9c11b0bfaa8fe6c7f
chieh <chiehandlu@icloud.com>
2016-01-28 20:00:04 +0800

    unknown key

--- config/initializers/carrier_wave.rb
+++ config/initializers/carrier_wave.rb
@@ -3,11 +3,9 @@ CarrierWave.configure do |config|
     config.storage :fog
     config.fog_credentials = {
       provider:              'AWS',
-      aws_access_key_id:     'xxxx',
-
-      aws_secret_access_key: 'xxxx',
-
-      region:                'Oregon'
+      aws_access_key_id: ENV['aws_access_key_id'],
+      aws_secret_access_key: ENV['aws_secret_access_key'],
+      region:                'us-west-2'
 
     }
     config.fog_directory  = 'chieh-store'
